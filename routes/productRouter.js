156716fbccac71715b09ff8379b33fd2d84e19fa
Dilani <dilanithathsarani2003@gmail.com>
2025-05-29 11:46:30 +0530

    product router updated

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -1,10 +1,12 @@
 import express from 'express';
-import { createProduct, deleteProduct, getProducts, updateProduct } from '../controllers/productController.js';
+import { createProduct, deleteProduct,getProductById, getProducts,searchProduct, updateProduct } from '../controllers/productController.js';
 
 const productRouter = express.Router();
 
 productRouter.post("/",createProduct)
 productRouter.get("/",getProducts)
+productRouter.get("/search/:id",searchProduct)
+productRouter.get("/:id",getProductById)
 productRouter.delete("/:productId",deleteProduct)
 productRouter.put("/:productId",updateProduct)
 
