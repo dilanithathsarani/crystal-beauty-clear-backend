3ad36dc7d610b309be98b7401c4174b71604422f
Dilani <dilanithathsarani2003@gmail.com>
2025-05-29 11:49:41 +0530

    index.js file updated

--- index.js
+++ index.js
@@ -6,8 +6,10 @@ import productRouter from './routes/productRouter.js';
 import verifyJWT from './middleware/auth.js';
 import orderRouter from './routes/orderRouter.js';
 import dotenv from 'dotenv';
+import cors from "cors";
 dotenv.config()
 const app = express();
+app.use(cors());
 
 mongoose.connect(process.env.MONGO_URL).then(
     ()=>{
@@ -17,13 +19,13 @@ mongoose.connect(process.env.MONGO_URL).then(
     ()=>{
         console.log("Connection failed");
     }
-)
+);
 
 
 
 
 app.use(bodyParser.json());
-app.use(verifyJWT)
+app.use(verifyJWT);
 
 
 
@@ -37,4 +39,4 @@ app.listen(5000,
     ()=>{
         console.log("Server is running on port 5000");
     }
-)
+);
