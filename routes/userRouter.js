f4c01bb37015130648b80a44b383f9d3de0f595f
Dilani <dilanithathsarani2003@gmail.com>
2025-05-29 11:47:54 +0530

    user router updated

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,9 +1,13 @@
 import express from "express";
-import { loginUser, saveUser } from "../controllers/userController.js";
+import { changePassword,getCurrentUser,googleLogin,loginUser, saveUser,sendOTP } from "../controllers/userController.js";
 
 const userRouter = express.Router();
 
 userRouter.post("/",saveUser)
 userRouter.post("/login",loginUser)
+userRouter.post("/google",googleLogin)
+userRouter.get("/current",getCurrentUser)
+userRouter.post("/sendMail", sendOTP)
+userRouter.post("/changePW", changePassword)
 
 export default userRouter;
