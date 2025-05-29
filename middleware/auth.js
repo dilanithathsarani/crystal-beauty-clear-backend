5062437d234ee165820026c8a6b968cbda7efa70
Dilani <dilanithathsarani2003@gmail.com>
2025-05-29 11:40:48 +0530

    middleware updated

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,4 +1,6 @@
 import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config()
 export default function verifyJWT (req,res,next){
     const header = req.header("Authorization");
     if(header != null){
